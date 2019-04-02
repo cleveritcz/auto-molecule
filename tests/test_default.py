6ad6df601eff77c74e7870bc0b8ec7bf6b2ebb07
Martin Smola <martin.smola@adminitis.cz>
2019-04-02 20:42:05 +0200

    Docker: Added parallel tests (Debian 8, Debian 9, CentOS 7)

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python env
+
 import os
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 def test_nginx_is_installed(host):
     nginx = host.package("nginx")
